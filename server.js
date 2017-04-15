f017fb21a7bf00ebaa42e799da81337f981245bd
wfuller <wfuller87@gmail.com>
2017-04-14 23:24:39 -0700

    Updating CORS

--- server.js
+++ server.js
@@ -11,6 +11,19 @@ mongoose.connect(config.database);
 var index = require('./routes/index');
 var users = require('./routes/users.routes');
 
+app.use(function(req,res,next){
+  // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.use(morgan('dev'));
 app.use(express.static(__dirname + '/app'));
 app.use(bodyParser.urlencoded({extended: true}));
