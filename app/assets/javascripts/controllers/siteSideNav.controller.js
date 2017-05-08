6d92f73bd0d235cbaf4739f2b95cfaa8af87ca14
wfuller <wfuller87@gmail.com>
2017-05-07 22:23:31 -0700

    Adding sidenav
    Adding sidenav and updating site-container

---
+++ app/assets/javascripts/controllers/siteSideNav.controller.js
@@ -0,0 +1,58 @@
+angular.module('meanEnergy').controller('SiteSideNavController',[
+    '$scope',
+    '$timeout',
+    '$mdSidenav',
+    '$location',
+    '$rootScope',
+function(
+    $scope,
+    $timeout,
+    $mdSidenav,
+    $location,
+    $rootScope){
+
+
+    $scope.previousState = function(){
+      if ($rootScope.previousState.url === '^'){
+        $location.path('/home');
+      }
+      else{
+        $location.path($rootScope.previousState.url);
+      }
+    };
+
+
+  var originatorEv;
+
+  $scope.toggleSideNav = buildDelayedToggler('site_sidenav');
+  function debounce(func, wait, context) {
+    var timer;
+    return function debounced() {
+      var context = $scope,
+          args = Array.prototype.slice.call(arguments);
+      $timeout.cancel(timer);
+      timer = $timeout(function() {
+        timer = undefined;
+        func.apply(context, args);
+      }, wait || 10);
+    };
+  }
+  function buildDelayedToggler(navID) {
+    return debounce(function() {
+      $mdSidenav(navID)
+        .toggle();
+    }, 200);
+  }
+  function buildToggler(navID) {
+    return function() {
+      $mdSidenav(navID)
+        .toggle();
+    };
+  }
+
+  $scope.closeSideNav = function () {
+    console.log('close the side nav');
+    $mdSidenav('site_sidenav').close();
+  }
+
+}]);
